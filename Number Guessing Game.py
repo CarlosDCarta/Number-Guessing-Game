0befedd28bd685fc1c1e7084c1e627cb6188b982
Carlosgit config --global user.email <Carlos>
2024-08-14 16:42:40 -0400

    added intro to game and ascii art

---
+++ Number Guessing Game.py
@@ -0,0 +1,13 @@
+import art
+#ask user for if hard mode or easy mode
+print(art.logo1)
+print("Welcome to the number guessing game!")
+print("You will guess a number from 1 to 100")
+selected_difficulty = input("Type 'easy' or 'hard' mode:\n")
+# adjust game for selected difficulty
+
+#ask user for a guess
+
+# process the guess and ask user for guess
+
+# output if they win or lose
